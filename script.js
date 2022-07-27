c9653131c8a8ee3926636ee93d1605091916cfa4
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-27 19:53:02 -0300

    Add funções map, filter e reduce

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 // Método Map ---------
-
 const arrayMap = [1, 2, 3, 4, 5]; //esse é o array que você terá que iterar
-
 // função callback
 // Ela apenas imprime o elemento na posição do index e de que array ele veio
 function callbackMap(element, index, array) {
   return `Número ${element} no index: ${index}, veio desse array: ${array}`;
 }
-
 function map(array, callback) {
-  //sua lógica
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i], i, array));
+  }
+  return newArray;
 }
-
-//console.table(map(arrayMap, callbackMap));
-
+console.log(map(arrayMap, callbackMap));
 //Método Filter ---------
-
 const arrayFilter = [1, 2, 3, 4, 5]; //esse é o array que você terá que iterar
-
 // função callback
 // Ela apenas imprime o elemento que for maior que 2
 function callbackFilter(element, index, array) {
@@ -25,26 +22,37 @@ function callbackFilter(element, index, array) {
     return true;
   }
 }
-
 function filter(array, callback) {
-  //sua lógica
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 }
-
-//console.log(filter(arrayFilter, callbackFilter));
-
+console.log(filter(arrayFilter, callbackFilter));
 //Método Reduce ---------
-
 const arrayReduce = [1, 2, 3, 4, 5]; //esse é o array que você terá que iterar
-
 // função callback
 // Ela apenas soma os valores, como um acumulador mesmo
 function callbackReduce(acumulator, valorAtual) {
   return acumulator + valorAtual;
 }
-
-function reduce(array, callback, initialValue = array[0]) {
-  //sua lógica
+function reduce(array, callback, initialValue) {
+  let indexInicial
+  let acumulador
+  if(initialValue === undefined) {
+    acumulador = array[0];
+    indexInicial = 1;  
+  } else {
+    acumulador = initialValue;
+    indexInicial = 0;
+  }
+  for (let i = indexInicial; i < array.length; i++) {
+    acumulador = callback(acumulador, array[i]);
+  }
+  return acumulador;
 }
-
-// console.log(reduce(arrayReduce, callbackReduce));
-// console.log(reduce(arrayReduce, callbackReduce, 50));
+console.log(reduce(arrayReduce, callbackReduce));
+console.log(reduce(arrayReduce, callbackReduce, 50));
